d8276225ee3478f2e74c8a88f659c74edfb81c94
Edvard Ahlsén <edvard.ahlsen@gmail.com>
2016-10-05 16:32:34 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Avalgo-tsp
+//
+//  Created by Edvard Ahlsén on 05/10/16.
+//  Copyright © 2016 Edvard Ahlsén. All rights reserved.
+//
+
+#include "main.hpp"
+#include <iostream>
+
+int main() {
+    std::cout << "hello world" << std::endl;
+}
---
+++ main.hpp
@@ -0,0 +1,14 @@
+//
+//  main.hpp
+//  Avalgo-tsp
+//
+//  Created by Edvard Ahlsén on 05/10/16.
+//  Copyright © 2016 Edvard Ahlsén. All rights reserved.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
